0def3af5e668776ed7b3f3646e5b39b2ae599ffc
kanda <mikomiko.lust.love@gmail.com>
2024-09-01 21:05:22 +0900

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from blog.models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=('id','title', 'postdate','category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+   list_display=('id','category_name')
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin) 
